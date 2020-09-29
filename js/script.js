1930bcf75e59522a3ce673e18d9ae6deb1d471c1
Mathias <mathiasmichaelweiss@gmail.com>
2020-09-29 21:51:57 +0200

    counter_1

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const number = document.querySelector('.num'),
+    plus = document.querySelector('.plus'),
+    minus = document.querySelector('.minus');
+
+function counter() {
+    let x = 0;
+
+    if (x <= 0) {
+        
+    }
+
+    plus.addEventListener('click', () => {
+        number.textContent = ++x;
+    });
+    minus.addEventListener('click', () => {
+        number.textContent = --x;
+    });
+
+}
+
+counter();
